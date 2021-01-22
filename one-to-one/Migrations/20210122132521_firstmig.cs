13dc31383238311d91075e1ac1d4b841c2de2ec1
afaqch1 <afaq.saeed@techverx.com>
2021-01-22 18:44:00 +0500

    first commit

---
+++ one-to-one/Migrations/20210122132521_firstmig.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace one_to_one.Migrations
+{
+    public partial class firstmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    studentid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    student_name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    student_age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.studentid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentAddresses",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    studentid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentAddresses", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_StudentAddresses_Students_studentid",
+                        column: x => x.studentid,
+                        principalTable: "Students",
+                        principalColumn: "studentid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentAddresses_studentid",
+                table: "StudentAddresses",
+                column: "studentid",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentAddresses");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ one-to-one/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+
+namespace one_to_many
+{
+    public class Student
+    {
+        public int studentid { get; set; }
+        public string student_name { get; set; }
+        public int student_age { get; set; }
+
+        public StudentAddress address { get; set; }
+    }
+
+    public class StudentAddress
+    {
+        public int id { get; set; }
+        public string address { get; set; }
+
+        public int studentid { get; set; }
+        public Student student { get; set; }
+        
+
+    }
+
+    public class ClassRoom : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentAddress> StudentAddresses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer(@"Server=.;Database=Db3;Trusted_Connection=True;");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new ClassRoom())
+            {
+                var std = new Student()
+                {
+                    student_name = "Afaq",
+                    student_age = 23
+                    
+                   
+
+                };
+                var std1 = new Student()
+                {
+                    student_name = "Yasim",
+                    student_age = 24
+                    
+                };
+                var std2 = new Student()
+                {
+                    student_name = "Wamik",
+                    student_age = 22
+                    
+                };
+                var std3 = new Student()
+                {
+                    student_name = "Umair",
+                    student_age = 99
+                    
+                };
+
+                var adr = new StudentAddress()
+                {
+
+                    address = "Township",
+                    studentid=2
+                    
+                    
+                };
+                var adr1 = new StudentAddress()
+                {
+                    address = "Mughalpura",
+                    studentid=1
+                };
+                var adr2 = new StudentAddress()
+                {
+                    address = "wapda town",
+                    studentid=3
+                };
+
+                context.Students.Add(std);
+                context.Students.Add(std1);
+                context.Students.Add(std2);
+                context.Students.Add(std3);
+
+
+                context.StudentAddresses.Add(adr);
+                context.StudentAddresses.Add(adr1);
+                context.StudentAddresses.Add(adr2);
+
+
+
+                  
+
+
+                context.SaveChanges();
+
+
+
+
+            }
+        }
+    }
+}
